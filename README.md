d039534e2ba7d4fe3669f385abbf4554ec3fee5c
Fabrizio99 <fabrizio.condori.guzman@gmail.com>
2022-04-16 22:36:48 -0500

    commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dribbble Clone
+
+Web page inspired on Dribbble built by using Tailwind. Just for learning purposes
+
+To run the project
+```
+npm run tw:dev
+```
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        primary: "#0D0C22",
+        secondary: "#9E9EA7",
+        third: "#ea4c89",
+        shadow: "#F3F3F4",
+        focus: "#FEEDF3",
+      },
+      borderColor: {
+        focus: "#FAB9D0",
+      },
+      padding: {
+        18: "72px",
+      },
+    },
+  },
+  plugins: [],
+};
